0e6c01be513d06f1d681d34ef7e86507faa545f2
雨宮 澪音 <amamiya_mion@aosc.io>
2026-01-21 21:59:45 +0800

    added Neovide configuration
    Signed-off-by: 雨宮 澪音 <amamiya_mion@aosc.io>

---
+++ lua/config/neovide.lua
@@ -0,0 +1,28 @@
+vim.o.guifont = "Maple Mono Normal NF:h12"
+vim.g.neovide_opacity = 0.97
+vim.g.neovide_normal_opacity = 0.97
+
+-- IME Settings
+-- for East Asian Languages
+
+local function set_ime(args)
+  if args.event:match("Enter$") then
+    vim.g.neovide_input_ime = true
+  else
+    vim.g.neovide_input_ime = false
+  end
+end
+
+local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+  group = ime_input,
+  pattern = "*",
+  callback = set_ime
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+  group = ime_input,
+  pattern = "[/\\?]",
+  callback = set_ime
+})
